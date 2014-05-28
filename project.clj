4f4ec88e586210d41875e221fbe3b7a64d63b523
Giorgos Tsiftsis <giorgos.tsiftsis@skroutz.gr>
2014-05-28 22:29:10 +0300

    Update project file

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject clj-skroutz "0.0.1-SNAPSHOT"
-  :description "Cool new project to do things and stuff"
-  :dependencies [[org.clojure/clojure "1.4.0"]]
+  :description "A Clojure client for Skroutz API (v3 and above)"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [clj-http "0.9.2"]
+                 [cheshire "5.3.1"]]
   :profiles {:dev {:dependencies [[midje "1.5.0"]]}})
-  
