c4ffc0243c26f0323822041f8710456375172e42
Giorgos Tsiftsis <giorgos.tsiftsis@skroutz.gr>
2014-06-02 14:43:38 +0300

    Update build-request function

--- src/clj_skroutz/core.clj
+++ src/clj_skroutz/core.clj
@@ -3,6 +3,8 @@
   (require [cheshire.core :as json]))
 
 (def ^:dynamic url "https://api.skroutz.gr/")
+(def ^:dynamic user-agent "clj-skroutz client")
+(def ^:dynamic accept-header "application/vnd.skroutz+json;version=3")
 
 (defn format-url
   ([end-point]
@@ -10,3 +12,30 @@
   ([end-point positional]
    (str url (apply format end-point positional))))
 
+(defn build-request
+  "Builds a request to Skroutz API using some sane defaults."
+  [method end-point positional &
+   {:strs [throw_exceptions follow_redirects accept
+           oauth_token user_agent]
+    :or {follow_redirects true throw_exceptions false
+         accept accept-header user_agent user-agent}
+    :as query}]
+  (let [req (merge-with merge
+                        {:url (format-url end-point positional)
+                         :throw-exceptions throw_exceptions
+                         :follow-redirects follow_redirects
+                         :method method}
+                        (when accept
+                          {:headers {"Accept" accept}})
+                        (when oauth_token
+                          {:headers {"Authorization" (str "Bearer " oauth_token)}})
+                        (when user_agent
+                          {:headers {"User-Agent" user_agent}})
+                        )]
+  req))
+
+(defn api-response
+  [{:keys [headers status body] :as response}]
+  (cond
+    (= 200 status)
+      (json/parse-string body)))
--- test/clj_skroutz/t_core.clj
+++ test/clj_skroutz/t_core.clj
@@ -2,9 +2,6 @@
   (:use midje.sweet)
   (:use [clj_skroutz.core]))
 
-(facts "about `build-request`"
-  (fact ))
-
 (facts "about `format-url`"
   (fact "it formats urls"
     (format-url "category")
@@ -14,3 +11,26 @@
     (format-url "categories/%s/manufacturers/%s" [42 12])
       => "https://api.skroutz.gr/categories/42/manufacturers/12"
     ))
+
+(facts "about `build-request`"
+  (fact "it builds requests using `format-url` sets default accept header"
+    (build-request :get "categories/%s" [10]) =>
+     {:follow-redirects true, :method :get, :throw-exceptions false,
+      :url "https://api.skroutz.gr/categories/10"
+      :headers {"Accept" "application/vnd.skroutz+json;version=3"
+                "User-Agent" "clj-skroutz client"}})
+
+  (fact "it sets correct oauth header when oauth_token is passed as param"
+    (:headers (build-request :get "categories/%s" [10], "oauth_token" "an-oauth-token"))
+      =>  {"Accept" "application/vnd.skroutz+json;version=3"
+           "Authorization" "Bearer an-oauth-token"
+           "User-Agent" "clj-skroutz client"})
+
+  (fact "it sets correct user-agent when user-agent is passed as param"
+    (:headers (build-request :get "categories/%s" [10], "user_agent" "a-user-agent"))
+      =>  {"Accept" "application/vnd.skroutz+json;version=3"
+           "User-Agent" "a-user-agent"}))
+
+
+
+
