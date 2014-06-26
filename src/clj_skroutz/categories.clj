a94b65c3bb5804b74a8f5da20b42a4468411e2db
Giorgos Tsiftsis <giorgos.tsiftsis@skroutz.gr>
2014-06-26 23:43:57 +0300

    Update comments in Category endpoints

--- src/clj_skroutz/categories.clj
+++ src/clj_skroutz/categories.clj
@@ -4,31 +4,43 @@
 
 (defn all
   "Lists all categories. Actually at the moment of writing this function lists
-   only the family categories"
+   only the family categories
+
+   http://docs.skroutz.gr/apiv3/v3/category/#list-all-categories"
    [& options]
    (skroutz/api-call :get "categories" [] options))
 
 (defn category
-  "Lists a single category by id"
+  "Lists a single category by id
+
+  http://docs.skroutz.gr/apiv3/v3/category/#retrieve-a-single-category"
   [id & options]
   (skroutz/api-call :get "categories/%s" [id] options))
 
 (defn parent
-  "Lists the parent category"
+  "Lists the parent category
+
+  http://docs.skroutz.gr/apiv3/v3/category/#retrieve-the-parent-of-a-category"
   [id & options]
   (skroutz/api-call :get "categories/%s/parent" [id] options))
 
 (defn root
-  "Lists the root category"
+  "Lists the root category
+
+  http://docs.skroutz.gr/apiv3/v3/category/#retrieve-the-root-category"
   [& options]
   (skroutz/api-call :get "categories/root" [] options))
 
 (defn children
-  "Lists the children of a category"
+  "Lists the children of a category
+
+  http://docs.skroutz.gr/apiv3/v3/category/#list-the-children-categories-of-a-category"
   [id & options]
   (skroutz/api-call :get "categories/%s/children" [id] options))
 
 (defn manufacturers
-  "Lists category manufacturers"
+  "Lists category manufacturers
+
+  http://docs.skroutz.gr/apiv3/v3/category/#list-a-categorys-manufacturers"
   [id & options]
   (skroutz/api-call :get "categories/%s/manufacturers" [id] options))
