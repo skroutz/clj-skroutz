b76008844981b50d4cbb6996d2cd1b6a662ff467
Giorgos Tsiftsis <giorgos.tsiftsis@skroutz.gr>
2014-08-19 15:17:41 +0300

    Add oauth/user-code and oauth/user-token

--- src/clj_skroutz/oauth.clj
+++ src/clj_skroutz/oauth.clj
@@ -7,7 +7,7 @@
 (defn app-token
   "Posts request for application token given client-id and client-secret."
   [& {:keys [client-id client-secret redirect-uri grant-type scope token-url
-           accept-header]
+             accept-header code]
     :or   {client-id (carica/config :client-id)
            client-secret (carica/config :client-secret)
            redirect-uri (carica/config :redirect-uri)
@@ -21,4 +21,31 @@
                    :client_secret client-secret,
                    :redirect_uri redirect-uri
                    :grant_type grant-type
-                   :scope scope}}))
+                   :scope scope
+                   :code code}}))
+
+(defn user-code
+  "Gets an authorization code for user specific actions. Examples are
+   notifications favorites and so on.
+
+   http://docs.skroutz.gr/apiv3/guides/authentication/"
+  [& {:keys [client-id client-secret redirect-uri grant-type scope token-url
+             accept-header code]
+    :or   {client-id (carica/config :client-id)
+           client-secret (carica/config :client-secret)
+           redirect-uri (carica/config :redirect-uri)
+           grant-type (carica/config :grant-type)
+           scope (carica/config :scope)
+           token-url (carica/config :token-url)
+           accept-header (carica/config :accept-header)}}]
+  (client/get "https://www.skroutz.gr/oauth2/authorizations/new"
+    {:follow-redirects false,
+     :query-params {:client_id client-id,
+                    :redirect_uri redirect-uri
+                    :scope "public favorites override_users_approval"
+                    :response_type "code"}}))
+
+(defn user-token
+  "Gets a user authentication token"
+  [code]
+  (app-token :code code, :grant-type "authorization_code"))
