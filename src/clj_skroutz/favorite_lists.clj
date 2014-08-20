0deb149f8b2f25277f1cac15fd01bf2b317d3a76
Giorgos Tsiftsis <giorgos.tsiftsis@skroutz.gr>
2014-08-20 10:50:52 +0300

    Add favorite lists endpoints

---
+++ src/clj_skroutz/favorite_lists.clj
@@ -0,0 +1,34 @@
+(ns clj_skroutz.favorite_lists
+  "Implements user favorite lists endpoints.
+
+   http://docs.skroutz.gr/apiv3/v3/favorites/"
+  (:use [clj_skroutz.core :refer [api-call]]))
+
+(defn all
+  "Lists all favorite lists of a user.
+
+   http://docs.skroutz.gr/apiv3/v3/favorites/#list-favorite-lists"
+   [& options]
+   (api-call :get "favorite_lists" [] options))
+
+(defn create
+  "Creates a new favorite list.
+
+   http://developer.skroutz.gr/api/v3/favorites/#create-a-favoritelist"
+   [name & options]
+   (api-call :post "favorite_lists" []
+             (conj options ["query_params" {"favorite_list[name]" name}])))
+
+(defn destroy
+  "Destroys a favorite list.
+
+   http://developer.skroutz.gr/api/v3/favorites/#destroy-a-favoritelist"
+  [id & options]
+  (api-call :delete "favorite_lists/%s" [id] options))
+
+(defn favorites
+  "Lists favorites from a list.
+
+   http://developer.skroutz.gr/api/v3/favorites/#list-favorites-belonging-to-list"
+  [id & options]
+  (api-call :get "favorite_lists/%s/favorites" [id] options))
