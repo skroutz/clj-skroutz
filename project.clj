1581ca0b26f4d9de86f5c2736bd372f88dcedbb6
Giorgos Tsiftsis <giorgos.tsiftsis@skroutz.gr>
2014-09-10 10:02:51 +0300

    Update project.clj

--- project.clj
+++ project.clj
@@ -5,5 +5,8 @@
                  [cheshire "5.3.1"]
                  [com.cemerick/url "0.1.1"]
                  [ring/ring-core "1.3.0"]]
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :url "https://github.com/skroutz/clj-skroutz"
   :profiles {:dev {:dependencies [[midje "1.6.3"]
                                   [sonian/carica "1.1.0"]]}})
